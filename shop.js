cb71fa5f9a4f07c35029f0d859149444bba26e26
berkelpam <paul@Pauls-MacBook-Air-3.local>
2026-06-10 20:15:37 +0200

    Revert "ticket #62"
    This reverts commit 0744d4183529d113954f1eda65c99459634cbab1.

--- shop.js
+++ shop.js
@@ -1,7 +1,6 @@
 const PRODUCTS = {
   apple: { name: "Apple", emoji: "🍏" },
   banana: { name: "Banana", emoji: "🍌" },
-  grapes: { name: "Grapes", emoji: "🍇" },
   lemon: { name: "Lemon", emoji: "🍋" },
   strawberry: { name: "Strawberry", emoji: "🍓" },
 };
@@ -72,22 +71,12 @@ function addToBasket(product) {
   // Prevent strawberries and bananas being combined
   const hasBanana = basket.includes("banana");
   const hasStrawberry = basket.includes("strawberry");
-  const hasGrapes = basket.includes("grapes");
   if (product === "strawberry" && hasBanana) {
     showError("Strawberries and bananas cannot be combined.");
     return;
   }
   if (product === "banana" && hasStrawberry) {
     showError("Strawberries and bananas cannot be combined.");
-    return;
-  }
-  // Prevent grapes and bananas being combined
-  if (product === "grapes" && hasBanana) {
-    showError("Grapes and bananas cannot be combined.");
-    return;
-  }
-  if (product === "banana" && hasGrapes) {
-    showError("Grapes and bananas cannot be combined.");
     return;
   }
   basket.push(product);
